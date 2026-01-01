0658f11a7644b1e142156bdeccb1a57e54f29a6c
Kmn-san <kumenoshouhei0612@gmail.com>
2026-01-01 11:23:14 +0900

    startServer method added

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,11 @@ if(ENV.NODE_ENV === "production"){
     })
 }
 
-app.listen(ENV.PORT, () => {
-    console.log(`Server is running on port ${ENV.PORT}`);
-    connectDB()
-})
+const startServer = async() => {
+    await connectDB();
+    app.listen(ENV.PORT,() =>{
+        console.log("Server is up and running");
+    })
+}
+
+startServer()
